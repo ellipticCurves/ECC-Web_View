b83007a201a2907a3b74b24c08a46666f3108e68
ellipticCurves <127581586+ellipticCurves@users.noreply.github.com>
2023-03-13 22:06:45 +0530

    Update settings.py

--- tedwards/settings.py
+++ tedwards/settings.py
@@ -31,10 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 #commented out the hosts for the time being for local development
-ALLOWED_HOSTS = [
-    # 'twisted-edwards-calculator.herokuapp.com',
-    # 'localhost'
-]
+ALLOWED_HOSTS = ['https://ellipticcurvecalculator.onrender.com/']
 
 
 # Application definition
